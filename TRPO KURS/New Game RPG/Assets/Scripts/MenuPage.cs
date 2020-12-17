b4c5059425a59e30c5db29b1555331f0bd90be2b
bezzhonovroman <60962979+bezzhonovroman@users.noreply.github.com>
2020-12-17 22:40:50 +0300

    1.0

---
+++ TRPO KURS/New Game RPG/Assets/Scripts/MenuPage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CanvasGroup))]
+public class MenuPage : MonoBehaviour
+{
+    private CanvasGroup canvasgroup;
+
+    void Start()
+    {
+        canvasgroup = GetComponent<CanvasGroup>();
+    }
+    public void Show()
+    {
+        canvasgroup.alpha = 1f;
+        canvasgroup.interactable = true;
+        canvasgroup.blocksRaycasts = true;
+    }
+
+    public void Hide()
+    {
+        canvasgroup.alpha = 0f;
+        canvasgroup.interactable = false;
+        canvasgroup.blocksRaycasts = false;
+    }
+}
---
+++ TRPO KURS/New Game RPG/Assets/Scripts/RecordUpdater.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordUpdater : MonoBehaviour
+{
+    public GameObject recordTemplate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int CountRecords = PlayerPrefs.GetInt("CountRecords");
+        int CurrentScoreCount = PlayerPrefs.GetInt("ScoreCount");
+        Debug.Log("CountRecords " + CountRecords);
+        Debug.Log("CurrentScoreCount " + CurrentScoreCount);
+        Debug.Log("CurrentName" + PlayerPrefs.GetString("CurrentName"));
+        if (CurrentScoreCount != -1)
+        {
+            PlayerPrefs.SetString("Name" + CountRecords, PlayerPrefs.GetString("CurrentName")); ;
+            PlayerPrefs.SetInt("Score" + CountRecords, CurrentScoreCount);
+            PlayerPrefs.SetInt("ScoreCount", -1);
+            PlayerPrefs.SetInt("CountRecords", ++CountRecords);
+        }
+        
+
+        for (int i = 0; i < CountRecords; i++)
+        {
+            string name = PlayerPrefs.GetString("Name" + i);
+            int ScoreCount = PlayerPrefs.GetInt("Score" + i);
+            GameObject createdRecord = Instantiate(recordTemplate, transform);
+            createdRecord.transform.SetAsFirstSibling();
+            RecordController recordController = createdRecord.GetComponent<RecordController>();
+            recordController.Name = name;
+            recordController.Score = ScoreCount;
+            
+        }
+    }
+    
+  
+}
